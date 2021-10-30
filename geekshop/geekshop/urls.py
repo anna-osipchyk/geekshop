8b33dbf3a56218cefd4ffcde74f74a9c7b6f8069
Anna <annaosipchik03@gmail.com>
2021-10-30 15:49:54 +0300

    Lesson 2.Templates

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 import mainapp.views as mainapp
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", mainapp.main),
-    path("products/", mainapp.products),
-    path("contacts/", mainapp.contact),
+    path("", mainapp.main, name="main"),
+    path("products/", mainapp.products, name="products"),
+    path("contact/", mainapp.contact, name="contact"),
 ]
