084a2fd000b17f5471c4a36d59d7a8a4ce722645
Anna Osipchik <82057714+anna-osipchyk@users.noreply.github.com>
2021-11-08 16:58:58 +0300

    Lesson 3 (#5)
    * Lesson 3. Django ORM start
    
    * Lesson 3. Djagno adminb & ORM
    
    * Lesson 3. Django ORM start
    
    * Lesson 3. Urls for app
    
    * Lesson 3. Code style fix

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=64, unique=True, verbose_name="имя")
+    description = models.TextField(verbose_name="описание", blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, related_name="product", on_delete=models.CASCADE)
+    name = models.CharField(max_length=128, verbose_name="имя")
+    image = models.ImageField(blank=True, upload_to="products_images")
+    short_desc = models.TextField(max_length=60, blank=True, verbose_name="краткое описание")
+    description = models.TextField(blank=True, verbose_name="описание")
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0, verbose_name="цена")
+    quantity = models.PositiveIntegerField(default=0, verbose_name="количество на складе")
+
+    def __str__(self):
+        return f"{self.name} ({self.category.name})"
