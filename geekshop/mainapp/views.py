c267b2331d27f0f25d36c00910d9e2ffed1efb6b
Anna <annaosipchik03@gmail.com>
2021-11-12 18:14:48 +0300

    Lesson 5. Categories

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 
 from .models import *
@@ -7,7 +7,7 @@ from .models import *
 
 def main(request):
     title = "главная"
-    products = Product.objects.all()
+    products = Product.objects.all()[:4]
     content = {"title": title, "media_url": settings.MEDIA_URL, "products": products}
     return render(request, "mainapp/index.html", content)
 
@@ -15,6 +15,21 @@ def main(request):
 def products(request, pk=None):
     title = "продукты"
     links_menu = ProductCategory.objects.all()
+    if pk is not None:
+        if pk==0:
+            products = Product.objects.all().order_by("price")
+            category = {"name": "все"}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by("price")
+        content = {
+            "title": title,
+            "links_menu": links_menu,
+            "category": category,
+            "products": products,
+            "media_url": settings.MEDIA_URL,
+        }
+        return render(request, "mainapp/products_list.html", content)
     same_products = Product.objects.all()
     content = {
         "title": title,
@@ -30,28 +45,7 @@ def products(request, pk=None):
 def contact(request):
     title = "о нас"
     visit_date = timezone.now()
-    locations = [
-        {"city": "Москва", "phone": "+7-888-888-8888", "email": "info@geekshop.ru", "address": "В пределах МКАД"},
-        {
-            "city": "Екатеринбург",
-            "phone": "+7-777-777-7777",
-            "email": "info_yekaterinburg@geekshop.ru",
-            "address": "Близко к центру",
-        },
-        {
-            "city": "Владивосток",
-            "phone": "+7-999-999-9999",
-            "email": "info_vladivostok@geekshop.ru",
-            "address": "Близко к океану",
-        },
-    ]
-    content = {"title": title, "visit_date": visit_date, "locations": locations}
-    return render(request, "mainapp/contact.html", content)
-
-
-def contact(request):
-    title = "о нас"
-    visit_date = timezone.now()
     locations = Contact.objects.all()
     content = {"title": title, "visit_date": visit_date, "locations": locations}
     return render(request, "mainapp/contact.html", content)
+
