1e5314bdb92989f59c800f6d3f608c4519c7c2c6
Anna <annaosipchik03@gmail.com>
2021-11-12 19:30:05 +0300

    Lesson 5. Basket counter

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -2,21 +2,33 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 
-from .models import *
+from basketapp.models import Basket
+
+from .models import Contact, Product, ProductCategory
 
 
 def main(request):
     title = "главная"
+
     products = Product.objects.all()[:4]
-    content = {"title": title, "media_url": settings.MEDIA_URL, "products": products}
+
+    content = {"title": title, "products": products, "media_url": settings.MEDIA_URL}
     return render(request, "mainapp/index.html", content)
 
 
 def products(request, pk=None):
     title = "продукты"
     links_menu = ProductCategory.objects.all()
+
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+        # or you can use this
+        # _basket = request.user.basket.all()
+        # print(f'basket / _basket: {len(_basket)} / {len(basket)}')
+
     if pk is not None:
-        if pk==0:
+        if pk == 0:
             products = Product.objects.all().order_by("price")
             category = {"name": "все"}
         else:
@@ -28,6 +40,7 @@ def products(request, pk=None):
             "category": category,
             "products": products,
             "media_url": settings.MEDIA_URL,
+            "basket": basket,
         }
         return render(request, "mainapp/products_list.html", content)
     same_products = Product.objects.all()
@@ -36,6 +49,7 @@ def products(request, pk=None):
         "links_menu": links_menu,
         "same_products": same_products,
         "media_url": settings.MEDIA_URL,
+        "basket": basket,
     }
     if pk:
         print(f"User select category: {pk}")
@@ -48,4 +62,3 @@ def contact(request):
     locations = Contact.objects.all()
     content = {"title": title, "visit_date": visit_date, "locations": locations}
     return render(request, "mainapp/contact.html", content)
-
