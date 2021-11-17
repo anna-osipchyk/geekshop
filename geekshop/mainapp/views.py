b097d7743724d1d518dbe60d5034c20729a5202e
Anna Osipchik <82057714+anna-osipchyk@users.noreply.github.com>
2021-11-17 18:30:47 +0300

    Lesson 5 (#7)
    * Lesson 5. Categories
    
    * Lesson 5. Basket app start
    
    * Lesson 5. Basket counter

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,52 +1,59 @@
 from django.conf import settings
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 
-from .models import *
+from basketapp.models import Basket
+
+from .models import Contact, Product, ProductCategory
 
 
 def main(request):
     title = "главная"
-    products = Product.objects.all()
-    content = {"title": title, "media_url": settings.MEDIA_URL, "products": products}
+
+    products = Product.objects.all()[:4]
+
+    content = {"title": title, "products": products, "media_url": settings.MEDIA_URL}
     return render(request, "mainapp/index.html", content)
 
 
 def products(request, pk=None):
     title = "продукты"
     links_menu = ProductCategory.objects.all()
+
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+        # or you can use this
+        # _basket = request.user.basket.all()
+        # print(f'basket / _basket: {len(_basket)} / {len(basket)}')
+
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by("price")
+            category = {"name": "все"}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by("price")
+        content = {
+            "title": title,
+            "links_menu": links_menu,
+            "category": category,
+            "products": products,
+            "media_url": settings.MEDIA_URL,
+            "basket": basket,
+        }
+        return render(request, "mainapp/products_list.html", content)
     same_products = Product.objects.all()
     content = {
         "title": title,
         "links_menu": links_menu,
         "same_products": same_products,
         "media_url": settings.MEDIA_URL,
+        "basket": basket,
     }
     if pk:
         print(f"User select category: {pk}")
     return render(request, "mainapp/products.html", content)
-
-
-def contact(request):
-    title = "о нас"
-    visit_date = timezone.now()
-    locations = [
-        {"city": "Москва", "phone": "+7-888-888-8888", "email": "info@geekshop.ru", "address": "В пределах МКАД"},
-        {
-            "city": "Екатеринбург",
-            "phone": "+7-777-777-7777",
-            "email": "info_yekaterinburg@geekshop.ru",
-            "address": "Близко к центру",
-        },
-        {
-            "city": "Владивосток",
-            "phone": "+7-999-999-9999",
-            "email": "info_vladivostok@geekshop.ru",
-            "address": "Близко к океану",
-        },
-    ]
-    content = {"title": title, "visit_date": visit_date, "locations": locations}
-    return render(request, "mainapp/contact.html", content)
 
 
 def contact(request):
