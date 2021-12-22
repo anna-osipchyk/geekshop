549b70002789dcfc1ca98996c3cec184685e230e
anna-osipchyk <annaosipchik03@gmail.com>
2021-12-22 23:14:08 +0300

    lesson_16. F and update

--- geekshop/adminapp/forms.py
+++ geekshop/adminapp/forms.py
@@ -12,6 +12,8 @@ class ShopUserAdminEditForm(ShopUserEditForm):
 
 
 class ProductCategoryEditForm(forms.ModelForm):
+    discount = forms.IntegerField(label="скидка", required=False, min_value=0, max_value=90, initial=0)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
@@ -21,7 +23,6 @@ class ProductCategoryEditForm(forms.ModelForm):
     class Meta:
         model = ProductCategory
         fields = "__all__"
-
 
 class ProductEditForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
